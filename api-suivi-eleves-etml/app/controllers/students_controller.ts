ea6aef9adc42915c11157522dfc71a68e37dd773
Brendan Fleurdelys <fbrend23@gmail.com>
2025-09-05 15:16:20 +0200

    add step 3 et 4

---
+++ api-suivi-eleves-etml/app/controllers/students_controller.ts
@@ -0,0 +1,62 @@
+import Student from '#models/student'
+import type { HttpContext } from '@adonisjs/core/http'
+
+export default class StudentsController {
+  /**
+   * Display a list of resource
+   */
+  async index({}: HttpContext) {
+    return await Student.query().orderBy('name').orderBy('firstname')
+  }
+
+  /**
+   * Display form to create a new record
+   */
+  async create({}: HttpContext) {}
+
+  /**
+   * Handle form submission for the create action
+   */
+  async store({ request }: HttpContext) {
+    const student = request.only(['name', 'firstname'])
+    return await Student.create(student)
+  }
+
+  /**
+   * Show individual record
+   */
+  async show({ params }: HttpContext) {
+    return await Student.findOrFail(params.id)
+  }
+
+  /**
+   * Edit individual record
+   */
+  // async edit({ params }: HttpContext) {}
+
+  /**
+   * Handle form submission for the edit action
+   */
+  async update({ params, request }: HttpContext) {
+    //Récupération des données
+    const data = request.only(['name', 'firstname'])
+
+    //Vérification de l'existence de l'élève
+    const student = await Student.findOrFail(params.id)
+
+    student.merge(data)
+
+    await student.save()
+
+    return student
+  }
+
+  /**
+   * Delete record
+   */
+  async destroy({ params }: HttpContext) {
+    const student = await Student.findOrFail(params.id)
+
+    return await student.delete()
+  }
+}
